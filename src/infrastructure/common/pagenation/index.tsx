4be7ecab3bcead6ef6ef607cf4081ed543d55fe8
MinSamJun <116451120+MinSamJun@users.noreply.github.com>
2025-04-15 18:13:22 +0900

    mhw: 방어구 그룹스킬 버그

--- src/infrastructure/common/pagenation/index.tsx
+++ src/infrastructure/common/pagenation/index.tsx
@@ -20,21 +20,27 @@ export function Pagination({
   const commonNamespace = getNamespaceData("common");
 
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-
   if (totalPages <= 1) return null;
+
+  const buttonClass = (disabled: boolean) =>
+    `px-4 py-2 rounded text-white ${
+      disabled
+        ? "bg-gray-400 cursor-not-allowed"
+        : "bg-blue-500 hover:bg-blue-600"
+    }`;
 
   return (
     <div className="flex justify-center mt-4 gap-4">
       <button
         onClick={onPrev}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
+        className={buttonClass(currentPage === 1)}
         disabled={currentPage === 1}
       >
         {commonNamespace?.common_prev_page}
       </button>
       <button
         onClick={onNext}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
+        className={buttonClass(currentPage === totalPages)}
         disabled={currentPage === totalPages}
       >
         {commonNamespace?.common_next_page}
