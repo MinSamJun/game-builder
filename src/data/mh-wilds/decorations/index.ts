c4e8662612ac3a4f070170d6993c2882543bdf47
MinSamJun <116451120+MinSamJun@users.noreply.github.com>
2025-05-07 01:50:10 +0900

    mhw:  타입 추론 오류 해결

--- src/data/mh-wilds/decorations/index.ts
+++ src/data/mh-wilds/decorations/index.ts
@@ -1,11 +1,4 @@
-import mhWildsArmorSkillDecorationDataRaw from "./mhwilds-armor-decorations.json";
-import mhWildsWeaponSkillDecorationDataRaw from "./mhwilds-weapon-decorations.json";
+import { mhWildsArmorSkillDecorationData } from "./mhwilds-armor-decorations";
+import { mhWildsWeaponSkillDecorationData } from "./mhwilds-weapon-decorations";
 
-import type { Decoration } from "@/types/mh-wilds";
-
-// 모든 장식주의 스킬레벨이 확실하다는 확신이 있기에 as unknown을 사용함
-export const mhWildsArmorSkillDecorationData =
-  mhWildsArmorSkillDecorationDataRaw as unknown as Decoration[];
-
-export const mhWildsWeaponSkillDecorationData =
-  mhWildsWeaponSkillDecorationDataRaw as unknown as Decoration[];
+export { mhWildsArmorSkillDecorationData, mhWildsWeaponSkillDecorationData };
